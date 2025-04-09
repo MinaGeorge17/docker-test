116a48a888e5acb911642d356ffe0227147bfdae
root <root@localhost.localdomain>
2025-04-09 16:26:51 +0200

    Rename to vars/dockerBuildPush.groovy for Jenkins

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,6 +1,4 @@
 def call(String imageName, String tag, String dockerfilePath = '.') {
-    // Build Docker image
     sh "docker build -t ${imageName}:${tag} ${dockerfilePath}"
-    // Push to Docker Hub
     sh "docker push ${imageName}:${tag}"
 }
